eac8c25b5aa294ae3cf225395edee30d0977d764
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-08-23 22:43:41 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,8 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -10,9 +12,10 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
-def genre=(genre)
+  def genre=(genre)
     @genre = genre
-    GENRES << genre 
+    GENRES << genre
+    #or GENRES = @genre
   end
-  
+
 end
